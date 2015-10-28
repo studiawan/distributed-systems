1e45cadf50ddeaf80294f565a7b4a1bffec25d40
studiawan <studiawan@gmail.com>
2015-10-28 07:39:31 +0700

    Create receive_log.py

---
+++ publish-subscribe/rabbitmq/receive_logs.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import pika
+import sys
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+channel.exchange_declare(exchange='logs',
+                         type='fanout')
+
+result = channel.queue_declare(exclusive=True)
+queue_name = result.method.queue
+
+channel.queue_bind(exchange='logs',
+                   queue=queue_name)
+
+print ' [*] Waiting for logs. To exit press CTRL+C'
+
+def callback(ch, method, properties, body):
+    print " [x] %r" % (body,)
+
+channel.basic_consume(callback,
+                      queue=queue_name,
+                      no_ack=True)
+
+try:
+    channel.start_consuming()
+
+except KeyboardInterrupt:
+    sys.exit(1)
